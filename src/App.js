38674c2bc1081783414d005ac9f7372f6dfcaef3
Christina Hunter <christinahunter@Christinas-MacBook-Pro.local>
2021-06-27 17:00:15 -0700

    Fixed some things up, added the popup module when Read button is clicked

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import AllStoriesPage from "./pages/AllStories";
 import CreateStoryPage from "./pages/CreateStory";
 import LikedStoriesPage from "./pages/LikedStories";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 import { useEffect, useState } from "react";
 import "./App.css";
 
@@ -27,21 +27,21 @@ function App() {
   // rendering mark up
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact={true}>
-          <AllStoriesPage />
-        </Route>
-        <Route path="/create-story">
-          <CreateStoryPage />
-        </Route>
-        <Route path="/liked-stories">
-          <LikedStoriesPage />
-        </Route>
-      </Switch>
-
+      <Layout>
       <h2>The date according to Go is:</h2>
       <p>{date ? date : "Loading date..."}</p>
+        <Switch>
+          <Route path="/" exact={true}>
+            <AllStoriesPage />
+          </Route>
+          <Route path="/create-story">
+            <CreateStoryPage />
+          </Route>
+          <Route path="/liked-stories">
+            <LikedStoriesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- src/components/stories/StoryItem.js
+++ src/components/stories/StoryItem.js
@@ -1,8 +1,25 @@
 import React from "react";
 import classes from "./StoryItem.module.css";
+import Backdrop from "../Backdrop";
+import Modal from "../Modal";
+import Card from "../ui/Card";
+import { useState } from "react";
+
 
 function StoryItem(props) {
+    const [modalIsOpen, setModalIsOpen]  = useState(false);
+
+
+    function readHandler(){
+        setModalIsOpen(true);
+    }
+
+    function closeModalHandler() {
+        setModalIsOpen(false);
+    }
+
   return (
+    <Card>
     <li className={classes.item}>
       <div className={classes.image}>
         <img src={props.image} alt={props.title} />
@@ -12,10 +29,13 @@ function StoryItem(props) {
         <h4>{props.author}</h4>
         <p>{props.summary}</p>
       </div>
-      <div className={classes.actions}>
+      <div className={classes.actions} onClick={readHandler}>
         <button>READ</button>
       </div>
+      {modalIsOpen ? <Modal onCancel={closeModalHandler} onViewFullStory={closeModalHandler}/> : null}
+     {modalIsOpen ? <Backdrop onCancel={closeModalHandler}/> : null}
     </li>
+    </Card>
   );
 }
 
